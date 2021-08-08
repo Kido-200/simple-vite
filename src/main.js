2d0762088cf4d565077318a26ffd3339e6199b89
chenweiyang <chenweiyang@bytedance.com>
2021-08-08 18:42:19 +0800

    final

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import {createApp, h} from 'vue'
-import App from './App.vue'
-import './index.css';
+const App = {
+  render() {
+    return h("div",null,[h("div",null,String('123'))]);
+  }
+}
 
 createApp(App).mount("#app");
